f1aa5825ce20dceaf0af69105a1858ad7009378a
AmosNico <75745529+AmosNico@users.noreply.github.com>
2022-11-05 08:08:37 +0100

    Update figures coplulas.R

--- figures coplulas.R
+++ figures coplulas.R
@@ -4,13 +4,12 @@ M <- mesh(seq(0, 1,length.out=10), seq(0, 1,length.out=10))
 u <- M$x
 v <- M$y
 
-
-plot_copula <- function(z,title,file_name) {
-  png(filename = file_name,height = 700, width = 700)
-  persp3D(u, v, z,colvar = NULL,facets = NA,border="black",theta =330,phi=30,
-          xlab = "u", ylab = "v", zlab = "z", main=title,
+plot_copula <- function(z, title, file_name) {
+  png(filename = file_name, height = 700, width = 700)
+  persp3D(u, v, z, colvar = NULL, facets = NA, border="black", theta =330, phi=30,
+          xlab = "u", ylab = "v", zlab = "z", main = title,
           cex.lab=2.5, cex.axis=2.5, cex.main=2.5) # enlargement of text
-  dev.off()
+  dev.off() # save png
 }
 
 # Pi(u,v)
@@ -21,7 +20,6 @@ plot_copula(pmin(u,v),"M(u,v)","3.2 max copula.png")
 
 # W(u,v)
 plot_copula(pmax(matrix(0,10,10),u+v-1),"W(u,v)","3.3 min copula.png")
-
 
 # Ali-Mikhail-Haq
 theta <- 1
